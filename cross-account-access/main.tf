b3810951f64b7e5c3a2ecd8388bfbd7a8cf00ecc
Zach Arguelles <admin@arguelles.me>
2025-05-06 22:42:24 -0400

    add security cross account role as well

--- cross-account-access/main.tf
+++ cross-account-access/main.tf
@@ -31,6 +31,37 @@ resource "aws_iam_role_policy" "cross_account_policy" {
   })
 }
 
+resource "aws_iam_role" "cross_account_role" {
+  name               = "CrossAccountDevRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Action    = "sts:AssumeRole"
+        Principal = {
+          AWS = "arn:aws:iam::${var.security_account_id}:role/OrganizationAccountAccessRole"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "cross_account_policy" {
+  name   = "CrossAccountPolicy"
+  role   = aws_iam_role.cross_account_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "sts:AssumeRole"
+        Resource = "arn:aws:iam::${var.security_account_id}:role/OrganizationAccountAccessRole"
+      }
+    ]
+  })
+}
+
 # Output role ARN for reference
 output "cross_account_role_arn" {
   value = aws_iam_role.cross_account_role.arn
--- cross-account-access/variables.tf
+++ cross-account-access/variables.tf
@@ -2,3 +2,8 @@ variable "dev_account_id" {
   description = "The AWS account ID for the Dev account"
   type        = string
 }
+
+variable "security_account_id" {
+  description = "The AWS account ID for the Dev account"
+  type        = string
+}
